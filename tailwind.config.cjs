aef49015cf3200c5b66c92c3c1a7165ab63dab35
Jered Leisey <jered.leisey@gmail.com>
2022-09-10 00:27:21 -0400

    custom tailwind plugin

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,5 +8,11 @@ module.exports = {
       },
     },
   },
-  plugins: [require('@tailwindcss/forms')],
+  plugins: [
+    require('@tailwindcss/forms'),
+    function ({ addVariant }) {
+      addVariant('child', '& > *');
+      addVariant('child-hover', '& > *:hover');
+    },
+  ],
 };
