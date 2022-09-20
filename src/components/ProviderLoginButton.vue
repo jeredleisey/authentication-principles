dad3e30577dcb49f09e2a28ddefe654b12393c51
Jered Leisey <jered.leisey@gmail.com>
2022-09-19 22:41:47 -0400

    created provider login button component

---
+++ src/components/ProviderLoginButton.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const emit = defineEmits(['login-with-provider']);
+const props = defineProps<{ provider: string }>();
+
+function buttonClick() {
+  emit('login-with-provider', props.provider);
+}
+</script>
+
+<template>
+  <button
+    class="bg-white border py-2 w-full rounded-md flex justify-center items-center text-sm hover:bg-yellow-50 focus:outline-none focus:ring-2 focus:ring-yellow-400 focus:ring-offset-2"
+    @click="buttonClick"
+  >
+    <img :src="`src/assets/${props.provider}.svg`" />
+  </button>
+</template>
