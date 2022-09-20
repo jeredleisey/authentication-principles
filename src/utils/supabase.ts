975086281eaf19464f86428572151b8e2b1ec794
Jered Leisey <jered.leisey@gmail.com>
2022-09-19 22:39:45 -0400

    moved auth into supabase util

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,6 +1,58 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient, Provider } from '@supabase/supabase-js';
+import router from '../router';
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+// login function
+const login = async (email: string, password: string) => {
+  try {
+    const { error } = await supabase.auth.signIn({
+      email: email,
+      password: password,
+    });
+    if (error) throw error;
+  } catch (error) {
+    alert((error as Error).message);
+  }
+};
+
+// login with provider
+const loginWithProvider = async (provider: Provider) => {
+  try {
+    const { error } = await supabase.auth.signIn({
+      provider: provider,
+    });
+    if (error) throw error;
+  } catch (error) {
+    alert((error as Error).message);
+  }
+};
+
+// logout function
+const logOut = async () => {
+  try {
+    const { error } = await supabase.auth.signOut();
+    if (error) throw error;
+    router.push('/');
+  } catch (error) {
+    alert((error as Error).message);
+  }
+};
+
+// create account
+const createAcct = async (email: string, password: string) => {
+  try {
+    const { error } = await supabase.auth.signUp({
+      email: email,
+      password: password,
+    });
+    if (error) throw error;
+  } catch (error) {
+    alert((error as Error).message);
+  }
+};
+
+export { supabase, login, loginWithProvider, logOut, createAcct };
