c0d8bbdfed566d25e873e90a3682e9578e5f451a
Jered Leisey <jered.leisey@gmail.com>
2023-01-16 19:38:05 -0500

    removed baseurl from router history config

--- src/router/index.ts
+++ src/router/index.ts
@@ -9,7 +9,7 @@ import ResetPassword from '@/components/ResetPassword.vue';
 import NotFound from '@/components/NotFound.vue';
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes: [
     {
       path: '/',
