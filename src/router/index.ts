1b4baf3f0530e3f534ffda466faaf24234af9daf
Jered Leisey <jered.leisey@gmail.com>
2023-01-16 20:56:49 -0500

    testing subdirectory

--- src/router/index.ts
+++ src/router/index.ts
@@ -9,7 +9,7 @@ import ResetPassword from '@/components/ResetPassword.vue';
 import NotFound from '@/components/NotFound.vue';
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory('/auth/'),
   routes: [
     {
       path: '/',
