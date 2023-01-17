a99fd3359b89c5f2e3c1bfcd0856136556e72847
Jered Leisey <jered.leisey@gmail.com>
2023-01-16 19:39:49 -0500

    removed base url from vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import * as path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/authentication-principles/',
+  // base: '/authentication-principles/',
   plugins: [vue()],
   resolve: {
     alias: { '@': path.resolve(__dirname, './src') },
