6123d6511df9d6b27faa6a1ce49fd7e1f76d76c8
Jered Leisey <jered.leisey@gmail.com>
2023-01-17 21:47:16 -0500

    setting subdirectory path

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import * as path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/auth/',
   plugins: [vue()],
   resolve: {
     alias: { '@': path.resolve(__dirname, './src') },
